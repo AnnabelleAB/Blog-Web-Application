291fc2615a50ce29bde510d30f6e5a5f153b3a94
AnnabelleAB <sunhuaqing01@gmail.com>
2022-03-01 11:57:57 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,94 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+const mongoose = require("mongoose");
+
+
+const homeStartingContent = "Oberservation, Documentation, Reflection.";
+const aboutContent = "Made by Annabelle Sun,deployed on MongoDB Altals and Heroku ";
+const contactContent = "sunhuaqing01@gmail.com";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+// connect mongoose
+
+mongoose.connect("mongodb+srv://annabelleshq:zzzxshq971001@cluster0.jyonr.mongodb.net/postsDB");
+
+const postSchema = {
+  title: String,
+  content: String
+};
+
+const Post = mongoose.model("Post", postSchema);
+
+
+app.get("/", function(req, res) {
+  Post.find({}, function(req, res) {
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts
+    });
+  })
+
+});
+
+app.get("/about", function(req, res) {
+  res.render("about", {
+    aboutContent: aboutContent
+  });
+});
+
+app.get("/contact", function(req, res) {
+  res.render("contact", {
+    contactContent: contactContent
+  });
+});
+
+app.get("/compose", function(req, res) {
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res) {
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+
+  posts.save(function(err) {
+    res.redirect("/");
+  });
+
+
+
+});
+
+
+
+app.get("/posts/:postId", function(req, res) {
+  const requestedPostId = req.params.postId;
+
+  Post.findOne({
+    _id: requestedPostId
+  }, function(err, post) {
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
+  });
+
+
+});
+
+app.listen(process.env.PORT || 3000, function(req, res) {
+  console.log("Server started on port 3000");
+});
